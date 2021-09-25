95942b0d7c824cdd76aa77efe4dc774eb715ad4f
Barufex <baruffa_89@hotmail.com>
2021-09-24 23:22:41 -0300

    entregable 2

--- mi-shop/src/components/NavBar/NavBar.js
+++ mi-shop/src/components/NavBar/NavBar.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import "./NavBar.css"
 import img from "../../assets/cart.jpg"
 
 
 const NavBar = ({brand}) => {
     return (
         <div>
-            <h1>{brand}</h1>
-            <img src= {img}></img>        
+            <h1 className= "NavBar">{brand}</h1>
+            <img src= {img} width= "60px"></img>
+            <ul>
+                <li><a href="https://google.com">Google</a></li>
+                <li><a href="https://hotmail.com">Hotmail</a></li>
+                <li><a href="https://saracatunga.com">Saracatunga</a></li>
+            </ul>        
         </div>
     )
 }
