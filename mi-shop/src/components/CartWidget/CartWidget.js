6507981bb0c91b9d6595af40a37e4389460adcc1
Barufex <baruffa_89@hotmail.com>
2021-09-26 18:42:00 -0300

    borrado de manifest

--- mi-shop/src/components/CartWidget/CartWidget.js
+++ mi-shop/src/components/CartWidget/CartWidget.js
@@ -4,7 +4,7 @@ import img2 from "../../assets/cart.jpg"
 const CartWidget = () => {
     return (
         <div>
-            <img src= {img2} width= "60px"></img>
+            <img src= {img2} width= "60px" alt= "Cart"></img>
         </div>
     )
 }
--- mi-shop/src/components/NavBar/NavBar.js
+++ mi-shop/src/components/NavBar/NavBar.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import "./NavBar.css"
-import img from "../../assets/cart.jpg"
 
 
 const NavBar = ({brand}) => {
     return (
         <div>
             <h1 className= "NavBar">{brand}</h1>
-            <img src= {img} width= "60px"></img>
             <ul>
                 <li><a href="https://google.com">Google</a></li>
                 <li><a href="https://hotmail.com">Hotmail</a></li>
